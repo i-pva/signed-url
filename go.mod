12f11819ca67aad78e79b4521e86743da9adcc50
Volodymyr <Volodymyr-pratsin@i.ua>
2020-11-16 22:09:28 +0200

    method implementations

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/i-pva/go-url
+
+go 1.15
+
+require github.com/google/uuid v1.1.2
--- url.go
+++ url.go
@@ -1,20 +1,58 @@
 package go_url
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
+	"github.com/google/uuid"
 	"net/url"
+	"strconv"
 	"time"
 )
 
+type KeyResolver func() string
+
 type Url struct {
 	url.URL
+	url.Values
+	KeyResolver KeyResolver
 }
 
-//Create a signed route URL for a path route.
-func (u *Url) SignedRoute() string {
-	return ""
+// Create a signed url URL for a path route.
+func (u *Url) Signed() string {
+	return u.sign(0)
 }
 
-// Create a temporary signed route URL for a path route.
-func (u *Url) TemporarySignedRoute(expiration time.Duration) string {
-	return ""
+// Create a temporary signed url URL for a path route.
+func (u *Url) TemporarySigned(expiration time.Duration) string {
+	return u.sign(expiration)
+}
+
+func (u *Url) sign(expiration time.Duration) string {
+	if u.KeyResolver == nil {
+		u.KeyResolver = func() string {
+			return uuid.New().String()
+		}
+	}
+
+	if expiration != 0 {
+		delay := time.Now().Add(expiration).Unix()
+		u.Values.Set("expires", strconv.Itoa(int(delay)))
+	}
+	u.Values.Set("signature", hash(u.bytes(), []byte(u.KeyResolver())))
+	u.RawQuery = u.Values.Encode()
+	return u.String()
+}
+
+// Func bytes returns bytes of url
+func (u *Url) bytes() []byte {
+	return []byte(u.String())
+}
+
+func hash(url, key []byte) string {
+	mac := hmac.New(sha256.New, key)
+	mac.Write(url)
+	byteArray := mac.Sum(nil)
+
+	return hex.EncodeToString(byteArray)
 }
